3692cd4b827024f886b1593309b3b928efef9886
Yijiao Wang <ttwyj250@gmail.com>
2021-07-28 07:33:41 -0700

    Urql

--- web/src/pages/register.tsx
+++ web/src/pages/register.tsx
@@ -1,6 +1,20 @@
 import React from 'react';
+import { useMutation } from 'urql';
 import { Formik, Form, FormikProps } from 'formik';
 import { InputField } from '../components/InputField';
+
+const REGISTER = `
+  mutation Register($username: String!, $password: String!) {
+    errors {
+      field
+      message
+    }
+    user {
+      id
+      username
+    }
+  }
+`;
 
 interface registerProps {}
 
@@ -10,11 +24,10 @@ interface Values {
 }
 
 const Register: React.FC<registerProps> = ({}) => {
+  const [, register] = useMutation(REGISTER);
+
   const handleSubmit = async (values, actions) => {
-    setTimeout(() => {
-      alert(JSON.stringify(values, null, 2));
-      actions.setSubmitting(false);
-    }, 1000);
+    const response = await register(values);
   };
 
   return (
