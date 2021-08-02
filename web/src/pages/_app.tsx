a4c8161abc6263cb6fb911260b0e6cc8986eeca1
Yijiao Wang <ttwyj250@gmail.com>
2021-08-01 17:30:48 -0700

    Update Urql cache after user register/login/logout

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,15 +1,61 @@
 import { ChakraProvider } from '@chakra-ui/react';
-
-import theme from '../theme';
 import { AppProps } from 'next/app';
-
-import { Provider, createClient } from 'urql';
+import { Provider, createClient, dedupExchange, fetchExchange } from 'urql';
+import { cacheExchange } from '@urql/exchange-graphcache';
+import { MeDocument } from '../generated/graphql';
+import theme from '../theme';
 
 const client = createClient({
   url: 'http://localhost:4000/graphql',
   fetchOptions: {
     credentials: 'include',
   },
+  exchanges: [
+    dedupExchange,
+    cacheExchange({
+      updates: {
+        Mutation: {
+          login: (result, args, cache, info) => {
+            cache.updateQuery({ query: MeDocument }, (data) => {
+              console.log('login result', result);
+              console.log('updateQuery data', data);
+
+              if (result.login.errors) {
+                return data;
+              } else {
+                return {
+                  me: result.login.user,
+                };
+              }
+            });
+          },
+
+          register: (result, args, cache, info) => {
+            cache.updateQuery({ query: MeDocument }, (data) => {
+              if (result.register.errors) {
+                return data;
+              } else {
+                return {
+                  me: result.register.user,
+                };
+              }
+            });
+          },
+
+          logout: (result, args, cache, info) => {
+            cache.updateQuery({ query: MeDocument }, (data) => {
+              if (result.logout.errors) {
+                return data;
+              } else {
+                return { me: null };
+              }
+            });
+          },
+        },
+      },
+    }),
+    fetchExchange,
+  ],
 });
 
 function MyApp({ Component, pageProps }: AppProps) {
